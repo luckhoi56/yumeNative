2ddedf724863be95e223fc00f36f9f49f8dd6815
Eric Luc <luckhoi89@gmail.com>
2020-12-13 13:50:54 -0800

    new commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes } from "@angular/router";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
-
+import {LoginComponent} from "./pages/login/login.component"
 const routes: Routes = [
+    //{ path: "", redirectTo: "/home", pathMatch: "full" },
+    //{ path: "home", loadChildren: () => import("~/app/home/home.module").then((m) => m.HomeModule) }
     { path: "", redirectTo: "/home", pathMatch: "full" },
-    { path: "home", loadChildren: () => import("~/app/home/home.module").then((m) => m.HomeModule) }
+    { path: "home", component: LoginComponent },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,22 @@ import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-
+import { TestComponent } from './test/test.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NativeScriptUISideDrawerModule } from "nativescript-ui-sidedrawer/angular";
 @NgModule({
     bootstrap: [
         AppComponent
     ],
     imports: [
         NativeScriptModule,
-        AppRoutingModule
+        AppRoutingModule,
+        NativeScriptUISideDrawerModule
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        TestComponent,
+        LoginComponent
     ],
     schemas: [
         NO_ERRORS_SCHEMA
