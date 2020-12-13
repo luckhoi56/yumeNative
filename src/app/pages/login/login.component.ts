0fbf07fcbba6cbd62c2a9562eb0729953e1e727f
Eric Luc <luckhoi89@gmail.com>
2020-12-13 00:11:50 -0800

    drawer added

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit, ChangeDetectorRef } from "@angular/core";
+import { RadSideDrawerComponent } from "nativescript-ui-sidedrawer/angular";
+import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+
 
 @Component({
   selector: 'ns-login',
@@ -6,10 +9,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  //xmlns:nsDrawer="nativescript-ui-sidedrawer"
+  
+  private _mainContentText: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private _changeDetectionRef: ChangeDetectorRef) {
   }
 
+  @ViewChild(RadSideDrawerComponent, { static: false }) public drawerComponent: RadSideDrawerComponent;
+  private drawer: RadSideDrawer;
+
+  ngAfterViewInit() {
+      this.drawer = this.drawerComponent.sideDrawer;
+      this._changeDetectionRef.detectChanges();
+  }
+
+  ngOnInit() {
+      this.mainContentText = "SideDrawer for NativeScript can be easily setup in the HTML definition of your page by defining tkDrawerContent and tkMainContent. The component has a default transition and position and also exposes notifications related to changes in its state. Swipe from left to open side drawer.";
+  }
+
+  get mainContentText() {
+      return this._mainContentText;
+  }
+
+  set mainContentText(value: string) {
+      this._mainContentText = value;
+  }
+
+  public openDrawer() {
+      this.drawer.showDrawer();
+  }
+
+  public onCloseDrawerTap() {
+      this.drawer.closeDrawer();
+  }
+  
 }
